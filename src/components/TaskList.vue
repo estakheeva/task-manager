42af8ef649a95dedeed507903e68ff5a2dd7fbc3
elestaada <kl.gogen@gmail.com>
2026-03-06 14:26:43 +0300

    Add 'Add Task' button and implement addTask logic using Pinia

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <button @click="addTask">Добавить задачу</button>
     <p v-for="task in tasks" :key="task.id">
       {{ task.title }} - {{ task.done ? 'Done' : 'In progress' }}
     </p>
@@ -11,11 +12,23 @@
 
   const tasksStore = useTasksStore()
   const tasks = tasksStore.tasks
+
+  // Add new task function
+function addTask() {
+  const newId = tasksStore.tasks.length + 1
+  tasksStore.addTask({ id: newId, title: `Новая задача ${newId}`, done: false })
+}
 </script>
 
 <style scoped>
 p {
   text-align: center;
   color: teal;
+}
+button {
+  display: block;
+  margin: 0 auto 1em;
+  padding: 0.5em 1em;
+  cursor: pointer;
 }
 </style>
