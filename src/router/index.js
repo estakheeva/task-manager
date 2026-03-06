44858aa26f0b97d6af1e2395d71ab60849866771
elestaada <kl.gogen@gmail.com>
2026-03-06 13:05:51 +0300

    Add route for Tasks.vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Login from '../views/Login.vue'
+import Tasks from '../views/Tasks.vue'
 
 const routes = [
   { path: '/', name: 'Login', component: Login },
+  { path: '/tasks', name: 'Tasks', component: Tasks },
 ]
 
 const router = createRouter({
