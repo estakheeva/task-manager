d243dfb5115991d5e614e1a5ee62d8a3a8830d8b
elestaada <kl.gogen@gmail.com>
2026-03-11 09:52:54 +0300

    Add Cypress test for Add task button

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -3,11 +3,16 @@ describe('Tasks page', () => {
   it('opens tasks page and shows tasks', () => {
 
     cy.visit('http://localhost:5173/tasks')
-
     cy.contains('Первая задача')
-
     cy.contains('Вторая задача')
+  })
 
+  it('adds a new task when clicking Add task', () => {
+    cy.visit('http://localhost:5173/tasks')
+
+    cy.get('button').click()
+
+    cy.contains('Новая')
   })
 
 })
